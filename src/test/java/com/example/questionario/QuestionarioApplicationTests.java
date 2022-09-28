38d5880a4f37cb8eacb1b4660c47f38eae76f48e
LorenzoPassalacqua <lorenzopassalacqua95@gmail.com>
2022-09-28 09:35:51 +0100

    primo commit

---
+++ src/test/java/com/example/questionario/QuestionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
